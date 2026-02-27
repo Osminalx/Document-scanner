044a6b419fccd949071d7cd90b4c5453b35e9d0f
Osminalx <verdestro64@gmail.com>
2026-02-26 20:21:13 -0800

    :construction: scanner init

--- scan.py
+++ scan.py
@@ -1,6 +1,34 @@
-def main():
-    print("Hello from pyscanner!")
+from pyimagesearch.transform import four_point_transform
+from skimage.filters import threshold_local
+import numpy as np
+import argparse
+import cv2
+import imutils
 
+# construct the argument parser and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", required=True, help="Path to the image to be scanned")
+args = vars(ap.parse_args())
 
-if __name__ == "__main__":
-    main()
+# load the image and compute the ratio of the old height
+# to the new height, clone it, and resize it
+# TODO: Check for a better way to manage if image is None
+image = cv2.imread(args["image"])
+if image is None:
+    raise ValueError(f"Could not load the image at path: {args['image']}")
+
+ratio = image.shape[0] / 500.0
+orig = image.copy()
+image = imutils.resize(image, height=500)
+
+# Convert the image to grayscale, blur it and find edges in the image
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+gray = cv2.GaussianBlur(gray, (5, 5), 0)
+edged = cv2.Canny(gray, 75, 200)
+
+# show the original image and the edge detected image
+print("STEP 1: Edge Detection")
+cv2.imshow("Image", image)
+cv2.imshow("Edged", edged)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
